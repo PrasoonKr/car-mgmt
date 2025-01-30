15a4553f8586f528e40545aaf29d87619bbfb38a
Prasoon Kumar <prasoon9582@gmail.com>
2025-01-30 19:38:32 +0530

    Adding backend files

---
+++ sampleCarData.js
@@ -0,0 +1,25 @@
+const sampleCars = [
+  {
+    title: "2020 Tesla Model 3",
+    description:
+      "Electric sedan in excellent condition. Autopilot enabled, premium interior.",
+    images: [
+      "https://dbhdyzvm8lm25.cloudfront.net/stills_0640_png/MY2020/14274/14274_st0640_116.png",
+      "https://dbhdyzvm8lm25.cloudfront.net/stills_0640_png/MY2020/14274/14274_st0640_059.png",
+      "https://dbhdyzvm8lm25.cloudfront.net/stills_0640_png/MY2020/14274/14274_st0640_119.png",
+    ],
+    tags: ["electric", "sedan", "tesla", "autopilot", "premium"],
+  },
+  {
+    title: "2019 Toyota Camry",
+    description:
+      "Well-maintained family sedan with low mileage. Includes backup camera and safety features.",
+    images: [
+      "https://d2ivfcfbdvj3sm.cloudfront.net/7fc965ab77efe6e0fa62e4ca1ea7673bb2594357021e3d8e88cb/stills_0640_png/MY2019/13269/13269_st0640_116.png",
+      "https://d2ivfcfbdvj3sm.cloudfront.net/7fc965ab77efe6e0fa62e4ca1ea7673bb2594357021e3d8e88cb/stills_0640_png/MY2019/13269/13269_st0640_037.png",
+    ],
+    tags: ["sedan", "toyota", "family", "economical"],
+  },
+];
+
+module.exports = sampleCars;
